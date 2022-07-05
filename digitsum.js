b1eef547f994b5f1ae9b04de6c41f9126c9cbb74
Amit Kumar <amitkumargang123@gmail.com>
2022-07-05 13:56:28 +0530

    iteration Assesment

---
+++ digitsum.js
@@ -0,0 +1,16 @@
+//Find the sum of the digits of a given number.
+
+//Write a program that takes a number from the user and get the sum of the digits present in the number.
+
+const Number_Sum = (N) => 
+{
+	let a,b = 0;
+	while (N){
+	  a = N % 10;
+	  b = b+a;
+	  N = parseInt (N/10);
+	}
+	return b;
+};
+
+console.log(Number_Sum(1234));
---
+++ numberdigit.js
@@ -0,0 +1,11 @@
+const Find_Digits = (N) => 
+{
+	let a = 0;
+	 while (N !== 0){
+	   N = parseInt(N / 10);
+	   a++;
+	 }
+	 return a;
+};
+console.log(Find_Digits(1234));
+
